fb06448d5c580b5fecbbf2c0a00a343f778205a4
Alex <software-as-solution@web.de>
2026-01-04 21:58:53 +0100

    Routen für app implementieren

--- next.config.js
+++ next.config.js
@@ -3,7 +3,20 @@ const nextConfig = {
   images: {
     domains: ['beafox.app'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/ios',
+        destination: 'https://apps.apple.com/app/id6746110612',
+        permanent: false,
+      },
+      {
+        source: '/android',
+        destination: 'https://play.google.com/store/apps/details?id=com.tapelea.beafox',
+        permanent: false,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
-
